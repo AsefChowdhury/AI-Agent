42c3474dd8a8820ecccffc477af4bd5a02c6b989
Asef Chowdhury <asefch0123@gmail.com>
2026-08-27 16:11:30 +0100

    feat: add bare bones Flask backend and JS frontend with hardcoded JSON exchange

--- backend/app.py
+++ backend/app.py
@@ -1,7 +1,18 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
+
+# GET = Get data, POST = send data
 
 @app.route("/")
 def home():
     return render_template('index.html')
+
+@app.route("/handle_post", methods = ['POST'])
+def handlePost():
+    data = request.json;
+    print(data)
+    return{
+        "note" : data["note"]
+    }
+        
