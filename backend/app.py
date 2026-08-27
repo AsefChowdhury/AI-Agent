e8811f17bda3e5a60ce8d5cd8fb599962ac8f6cf
Asef Chowdhury <asefch0123@gmail.com>
2026-08-27 18:38:06 +0100

    feat: connect Flask backend to local Ollama API

--- backend/app.py
+++ backend/app.py
@@ -1,6 +1,8 @@
 from flask import Flask, render_template, request
+import requests
 
 app = Flask(__name__)
+
 
 # GET = Get data, POST = send data
 
@@ -11,8 +13,17 @@ def home():
 @app.route("/handle_post", methods = ['POST'])
 def handlePost():
     data = request.json;
-    print(data)
-    return{
-        "note" : data["note"]
-    }
-        
+
+    payload = {
+                'model' : 'mistral',
+               'prompt' : data["note"],
+               'stream' : False}
+    url = "http://localhost:11434/api/generate"
+
+    response = requests.post(url, json=payload)
+
+    return response.json();
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
